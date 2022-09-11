07f5049d9bbb003ac8de5bda2b41dae099c5defb
symonf80 <94631730+symonf80@users.noreply.github.com>
2022-09-11 18:37:57 +0300

    Create Security.java

---
+++ src/main/java/com/example/daohibernate/Security.java
@@ -0,0 +1,38 @@
+package com.example.daohibernate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Security extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Bil")
+                .password(encoder().encode("1234"))
+                .authorities("read", "write")
+                .and()
+                .withUser("Bred")
+                .password("{noop}5678")
+                .authorities("read");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests().antMatchers("/hi").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
