edf73a0c86ec715b0f29d832028512e4d8842d8a
symonf80 <94631730+symonf80@users.noreply.github.com>
2022-09-12 16:34:32 +0300

    Update Security.java

--- src/main/java/com/example/daohibernate/Security.java
+++ src/main/java/com/example/daohibernate/Security.java
@@ -1,25 +1,19 @@
 package com.example.daohibernate;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Security extends WebSecurityConfigurerAdapter {
-    @Bean
-    public PasswordEncoder encoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+    
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Bil")
-                .password(encoder().encode("1234"))
+                .password("{noop}1234")
                 .authorities("read", "write")
                 .and()
                 .withUser("Bred")
